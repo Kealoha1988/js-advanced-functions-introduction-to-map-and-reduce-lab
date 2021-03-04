7bd1df891ed37ef4b2011da7ea3cb9eb91a9bccd
Kealoha1988 <kealohaabe1988@gmail.com>
2021-03-04 12:25:45 -0500

    commit tree 4e435bbc8debee448352711ef988c7fa41cf3763

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+
+// - `mapToNegativize(sourceArray)`
+// - `mapToNoChange(sourceArray)`
+// - `mapToDouble(sourceArray)`
+// - `mapToSquare(sourceArray)`
+
+let mapToNegativize = (array) => {
+  a = []
+  a.push(array.forEach(element => {element = ~element + 1
+  }))
+  return a
+}
+
+let mapToNochange = (array) => {
+ 
+  return array
+}
